1cb9589930560a3f81428ac0dbcaf09f764507f5
HeddaMarie <43143780+HeddaMarie@users.noreply.github.com>
2022-02-10 21:50:54 +0100

    Create devops.sh

---
+++ devops.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+# base operations
+apt-get update
+apt upgrade -y
+# install required packages
+apt install subversion -y
+apt install emacs -y
+apt install jed -y
+apt install git -y
+# add specified users
+adduser tim --disabled-password --shell /bin/bash
+adduser bob --disabled-password --shell /bin/bash
+adduser janet --disabled-password --shell /bin/bash
+adduser alice --disabled-password --shell /bin/bash
+# grant specified users root access
+usermod -a -G sudo tim
+usermod -a -G sudo alice
+usermod -a -G sudo bob
+usermod -a -G sudo janet
+# create required group(s) and add users to required group(s)
+groupadd developers 
+usermod -a -G developers ti
+usermod -a -G developers janet
