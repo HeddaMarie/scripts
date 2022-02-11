411db8df323462488ff7fcbf965092de24b94661
ghsol <43143776+ghsol@users.noreply.github.com>
2022-02-11 15:21:39 +0100

    Create devcompile.sh

---
+++ devcompile.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+# base operations
+apt-get update
+apt upgrade -y
+# install required packages
+apt install subversion -y
+apt install emacs -y
+apt install jed -y
+apt install git -y
+# Install compile-utilities 
+apt install make -y
+apt install binutils -y
+apt install gcc -y
+# add specified users
+adduser tim --disabled-password --shell /bin/bash
+adduser bob --disabled-password --shell /bin/bash
+adduser janet --disabled-password --shell /bin/bash
+adduser alice --disabled-password --shell /bin/bash
+# grant specified users root access
+usermod -a -G sudo tim
+usermod -a -G sudo alice
+usermod -a -G sudo bob
+usermod -a -G sudo janet
+# create required group(s) and add users to required group(s)
+groupadd developers 
+usermod -a -G developers tim
+usermod -a -G developers janet
