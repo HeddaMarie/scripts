8605aaea971b05723a7a68a05f62fbf6787fbf1f
HeddaMarie <43143780+HeddaMarie@users.noreply.github.com>
2022-02-25 21:51:40 +0100

    Update agent.sh

--- agent.sh
+++ agent.sh
@@ -2,7 +2,7 @@ apt-get update
 wget http://apt.puppetlabs.com/puppet6-release-focal.deb 
 dpkg -i puppet6-release-focal.deb 
 apt update
-sleep 10
+sleep 20
 apt -y install puppet-agent
 echo "10.0.0.111 tf-puppet.sparkles.gg tf-puppet" >> /etc/hosts
 /opt/puppetlabs/puppet/bin/puppet apply agent.pp
