6668e10a63a2d7cc21830ad7d5c5de3e72b145ed
HeddaMarie <43143780+HeddaMarie@users.noreply.github.com>
2022-02-25 19:56:42 +0100

    Update foreman.sh

--- foreman.sh
+++ foreman.sh
@@ -2,8 +2,10 @@
 wget http://apt.puppetlabs.com/puppet6-release-focal.deb
 dpkg -i puppet6-release-focal.deb
 apt update
-echo "deb http://deb.theforeman.org/ focal 3.0" >> /etc/apt/sources.list.d/foreman.list
-echo "deb http://deb.theforeman.org/ plugins 3.0" >> /etc/apt/sources.list.d/foreman.list
+apt-get -y install augeas-tools facter
 apt-get install ca-certificates -y
 wget -q https://deb.theforeman.org/pubkey.gpg -O- | apt-key add -
-apt update && apt upgrade && apt install foreman-installer
+
+echo "deb http://deb.theforeman.org/ focal 3.0" >> /etc/apt/sources.list.d/foreman.list
+echo "deb http://deb.theforeman.org/ plugins 3.0" >> /etc/apt/sources.list.d/foreman.list
+apt install foreman-installer
