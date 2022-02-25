de7ac4585037d03c82ad2575266f9f0ad7c394e7
HeddaMarie <43143780+HeddaMarie@users.noreply.github.com>
2022-02-25 20:10:36 +0100

    Update foreman.sh

--- foreman.sh
+++ foreman.sh
@@ -8,4 +8,6 @@ wget -q https://deb.theforeman.org/pubkey.gpg -O- | apt-key add -
 
 echo "deb http://deb.theforeman.org/ focal 3.0" >> /etc/apt/sources.list.d/foreman.list
 echo "deb http://deb.theforeman.org/ plugins 3.0" >> /etc/apt/sources.list.d/foreman.list
+apt update
+sleep 10
 apt install foreman-installer
