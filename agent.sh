c73d47a893e7ea237cf44f3c72183cdf956fab03
ghsol <43143776+ghsol@users.noreply.github.com>
2022-02-28 20:22:14 +0100

    Update agent.sh

--- agent.sh
+++ agent.sh
@@ -3,6 +3,7 @@ wget http://apt.puppetlabs.com/puppet6-release-focal.deb
 dpkg -i puppet6-release-focal.deb 
 apt update
 sleep 20
+wget https://raw.githubusercontent.com/HeddaMarie/scripts/main/agent.pp
 apt -y install puppet-agent
 echo "10.0.65.250 puppet.local" >> /etc/hosts
 /opt/puppetlabs/puppet/bin/puppet apply agent.pp
