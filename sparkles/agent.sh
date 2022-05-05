bc8f2859511954a01821f20a586200d4b4dda41a
HeddaMarie <43143780+HeddaMarie@users.noreply.github.com>
2022-05-05 20:15:59 +0200

    Update agent.sh

--- sparkles/agent.sh
+++ sparkles/agent.sh
@@ -3,7 +3,7 @@ wget http://apt.puppetlabs.com/puppet6-release-focal.deb
 dpkg -i puppet6-release-focal.deb 
 apt update
 sleep 20
-wget https://raw.githubusercontent.com/HeddaMarie/scripts/main/agent.pp
+wget https://raw.githubusercontent.com/HeddaMarie/scripts/sparkles/main/agent.pp
 apt -y install puppet-agent
 echo "10.0.0.3 puppet.sparkles.gg" >> /etc/hosts
 /opt/puppetlabs/puppet/bin/puppet apply agent.pp
