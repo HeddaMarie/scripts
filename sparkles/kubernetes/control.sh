adff27181f7d3d97968249f99cfbe875ad2c5aa3
HeddaMarie <43143780+HeddaMarie@users.noreply.github.com>
2022-05-08 23:47:40 +0200

    Update control.sh

--- sparkles/kubernetes/control.sh
+++ sparkles/kubernetes/control.sh
@@ -30,22 +30,6 @@ apt-mark hold kubelet kubeadm kubectl
 
 sleep 10
 
-#! /bin/bash
-
-apt update
-apt upgrade -y
-apt install -y  apt-transport-https ca-certificates curl gnupg lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-sudo usermod -aG docker $USER
-newgrp docker
-
 cat <<EOF | sudo tee /etc/containerd/config.toml
 
 #   Copyright 2018-2022 Docker Inc.
