c92c456ced06bf3c8a3febbed8c4146360bfe94a
HeddaMarie <43143780+HeddaMarie@users.noreply.github.com>
2022-05-08 23:59:31 +0200

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -9,8 +9,7 @@ echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] 
 
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+#curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#chmod +x /usr/local/bin/docker-compose
 sudo usermod -aG docker $USER
 newgrp docker
-exit
