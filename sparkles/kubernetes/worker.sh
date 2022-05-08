e31b6a58e435517f8542c2d7c1e249993594678c
HeddaMarie <43143780+HeddaMarie@users.noreply.github.com>
2022-05-08 23:36:51 +0200

    Update worker.sh

--- sparkles/kubernetes/worker.sh
+++ sparkles/kubernetes/worker.sh
@@ -24,11 +24,11 @@ sysctl --system
 
 
 # Step 3, change cgroupdriver for docker, restart docker.
-cat <<EOF | sudo tee /etc/docker/daemon.json
-{
-    "exec-opts": ["native.cgroupdriver=systemd"]
-}
-EOF
+#cat <<EOF | sudo tee /etc/docker/daemon.json
+#{
+#    "exec-opts": ["native.cgroupdriver=systemd"]
+#}
+#EOF
 
 systemctl restart docker
 
@@ -49,35 +49,33 @@ apt-get install -y apt-transport-https ca-certificates curl
 apt install kubeadm kubelet -y
 systemctl enable --now kubelet
 
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+sudo usermod -aG docker $USER
+newgrp docker
+
 cat <<EOF | sudo tee /etc/containerd/config.toml
-
 #   Copyright 2018-2022 Docker Inc.
-
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
 #   You may obtain a copy of the License at
-
 #       http://www.apache.org/licenses/LICENSE-2.0
-
 #   Unless required by applicable law or agreed to in writing, software
 #   distributed under the License is distributed on an "AS IS" BASIS,
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-
-
 #disabled_plugins = ["cri"]
-
 #root = "/var/lib/containerd"
 #state = "/run/containerd"
 #subreaper = true
 #oom_score = 0
-
 #[grpc]
 #  address = "/run/containerd/containerd.sock"
 #  uid = 0
 #  gid = 0
-
 #[debug]
 #  address = "/run/containerd/debug.sock"
 #  uid = 0
