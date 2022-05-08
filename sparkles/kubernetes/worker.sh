25c7e7e72b2d7e6eb97a88c3ee3bef81b591b2ac
HeddaMarie <43143780+HeddaMarie@users.noreply.github.com>
2022-05-08 23:35:28 +0200

    Update worker.sh

--- sparkles/kubernetes/worker.sh
+++ sparkles/kubernetes/worker.sh
@@ -49,6 +49,43 @@ apt-get install -y apt-transport-https ca-certificates curl
 apt install kubeadm kubelet -y
 systemctl enable --now kubelet
 
+cat <<EOF | sudo tee /etc/containerd/config.toml
+
+#   Copyright 2018-2022 Docker Inc.
+
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+
+#       http://www.apache.org/licenses/LICENSE-2.0
+
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+
+#disabled_plugins = ["cri"]
+
+#root = "/var/lib/containerd"
+#state = "/run/containerd"
+#subreaper = true
+#oom_score = 0
+
+#[grpc]
+#  address = "/run/containerd/containerd.sock"
+#  uid = 0
+#  gid = 0
+
+#[debug]
+#  address = "/run/containerd/debug.sock"
+#  uid = 0
+#  gid = 0
+#  level = "info"
+EOF
+
+
 # Ready to join cluster. 
 
 echo "run kubeadm token create --print-join-command on kube-master to get join token"
