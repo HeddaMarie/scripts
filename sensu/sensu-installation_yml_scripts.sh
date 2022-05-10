6300d949f68235e1423d7f5e55e365a0c72a5719
ghsol <43143776+ghsol@users.noreply.github.com>
2022-05-10 09:30:48 +0200

    Update sensu-installation_yml_scripts.sh
    Teste om at alt kan skje uten manuelle kommandoer

--- sensu/sensu-installation_yml_scripts.sh
+++ sensu/sensu-installation_yml_scripts.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+# docker.sh
+wget https://raw.githubusercontent.com/HeddaMarie/scripts/main/docker.sh
+
 # docker-compose.yml
 wget https://raw.githubusercontent.com/HeddaMarie/scripts/main/sensu/docker-compose.yml
 
@@ -10,3 +13,11 @@ wget https://raw.githubusercontent.com/HeddaMarie/scripts/main/sensu/sensu-backe
 
 # monitoring.sh
 wget https://raw.githubusercontent.com/HeddaMarie/scripts/main/sensu/monitoring.sh
+
+# Give permissions
+sudo chmod +x sensu-backend.sh monitoring.sh
+
+# Start scripts
+sudo ./sensu-backend.sh
+sudo ./monitoring.sh
+
