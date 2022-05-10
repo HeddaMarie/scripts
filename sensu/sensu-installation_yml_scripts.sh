1161dd0d7493dcd280edc7c0b85b60495a740a4b
ghsol <43143776+ghsol@users.noreply.github.com>
2022-05-10 09:20:09 +0200

    Create sensu-installation_yml_scripts.sh

---
+++ sensu/sensu-installation_yml_scripts.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+# docker-compose.yml
+wget https://raw.githubusercontent.com/HeddaMarie/scripts/main/sensu/docker-compose.yml
+
+# influxdb.yml
+wget https://raw.githubusercontent.com/HeddaMarie/scripts/main/sensu/influxdb.yml
+
+# sensu-backend.sh
+wget https://raw.githubusercontent.com/HeddaMarie/scripts/main/sensu/sensu-backend.sh
+
+# monitoring.sh
+wget https://raw.githubusercontent.com/HeddaMarie/scripts/main/sensu/monitoring.sh
