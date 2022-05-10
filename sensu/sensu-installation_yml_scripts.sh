547a780b6fd36ecdda7bfcba731de49d7b806191
ghsol <43143776+ghsol@users.noreply.github.com>
2022-05-10 10:17:10 +0200

    Update sensu-installation_yml_scripts.sh

--- sensu/sensu-installation_yml_scripts.sh
+++ sensu/sensu-installation_yml_scripts.sh
@@ -15,16 +15,12 @@ wget https://raw.githubusercontent.com/HeddaMarie/scripts/main/sensu/docker-comp
 # influxdb.yml
 wget https://raw.githubusercontent.com/HeddaMarie/scripts/main/sensu/influxdb.yml
 
-# sensu-backend.sh
-wget https://raw.githubusercontent.com/HeddaMarie/scripts/main/sensu/sensu-backend.sh
-
 # monitoring.sh
 wget https://raw.githubusercontent.com/HeddaMarie/scripts/main/sensu/monitoring.sh
 
 # Give permissions
-sudo chmod +x agent.sh docker.sh sensu-backend.sh monitoring.sh
+sudo chmod +x agent.sh docker.sh monitoring.sh
 
 # Start scripts
 sudo ./agent.sh
-sudo ./sensu-backend.sh
 sudo ./monitoring.sh
