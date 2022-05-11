581ba443d7b9eeca1cd11516287d6f704bf44623
ghsol <43143776+ghsol@users.noreply.github.com>
2022-05-11 11:01:05 +0200

    Update sensu-installation_yml_scripts.sh

--- sensu/sensu-installation_yml_scripts.sh
+++ sensu/sensu-installation_yml_scripts.sh
@@ -9,16 +9,17 @@ wget https://raw.githubusercontent.com/HeddaMarie/scripts/main/agent.sh
 # docker.sh
 wget https://raw.githubusercontent.com/HeddaMarie/scripts/main/docker.sh
 
-# docker-compose.yml
+# docker-compose.yml, source: https://oslomet.instructure.com/courses/23075/files/2636846?module_item_id=416082
 wget https://raw.githubusercontent.com/HeddaMarie/scripts/main/sensu/docker-compose.yml
 
 # sensu-daemonset.yml, source: https://github.com/sensu/sensu-k8s-quick-start/blob/master/kubernetes/sensu-daemonset.yaml
 wget https://raw.githubusercontent.com/HeddaMarie/scripts/main/sensu/sensu-daemonset.yml
 
-# influxdb.yml
+# influxdb.yml, source: https://oslomet.instructure.com/courses/23075/files/2636990?module_item_id=416151
 wget https://raw.githubusercontent.com/HeddaMarie/scripts/main/sensu/influxdb.yml
 
-# monitoring.sh
+# monitoring.sh, source: https://oslomet.instructure.com/courses/23075/files/2636990?module_item_id=416151 and
+# source: https://grafana.com/docs/grafana/latest/installation/debian/ and https://www.vultr.com/docs/how-to-install-influxdb-on-ubuntu-20-04/  
 wget https://raw.githubusercontent.com/HeddaMarie/scripts/main/sensu/monitoring.sh
 
 # Give permissions
